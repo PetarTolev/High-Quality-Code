5f6e7d05d7f8b059096474083de05dc4f960b4dc
PetarTolev <petar.dtolev@gmail.com>
2020-01-25 13:06:21 +0200

    Add exam

---
+++ 07. Exam/Zombow/Core/CommandPattern/CommandInterpreter.cs
@@ -0,0 +1,41 @@
+﻿namespace Zombow.Core.CommandPattern
+{
+    using Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Zombow.Core.Contracts;
+
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        private readonly IController controller;
+
+        public CommandInterpreter(IController controller)
+        {
+            this.controller = controller;
+        }
+
+        public string ProcessInput(IList<string> inputParameters)
+        {
+            string command = inputParameters[0].ToLower();
+            inputParameters.RemoveAt(0);
+
+            var method = this.controller
+                .GetType()
+                .GetMethods()
+                .FirstOrDefault(m => m.Name.ToLower().Contains(command));
+
+            if (method == null)
+            {
+                throw new ArgumentNullException("Command does not exist!");
+            }
+
+            if (method.GetParameters().Length > 0)
+            {
+                return method.Invoke(controller, new object[] {inputParameters}).ToString();
+            }
+
+            return method.Invoke(controller, new object[] {}).ToString();
+        }
+    }
+}
